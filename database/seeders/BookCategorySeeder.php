3c42cff261f5e3f390cb24584b72c0594100b2ea
superiorkid <mohammad.ilhamuddin@gmail.com>
2025-08-14 08:43:34 +0800

    book category model

---
+++ database/seeders/BookCategorySeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\BookCategory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BookCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        BookCategory::factory()->count(3000)->create();
+    }
+}
